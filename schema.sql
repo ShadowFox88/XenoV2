1fef950b751326b42d7348007248afc8d22a7cd2
Vahin Mehra <vahin@vahin.dev>
2024-03-24 01:42:00 +0000

    Fix schema.sql

--- schema.sql
+++ schema.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TYPE blacklist_type AS ENUM ('user', 'guild');
 
-CREATE TABLE IF NOT EXISTS blacklist {
+CREATE TABLE IF NOT EXISTS blacklist (
     id BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     name text NOT NULL,
     type blacklist_type NOT NULL,
     blacklisted_on timestamp with time zone NOT NULL default now()
     blacklisted_until timestamp with time zone NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS guilds {
+CREATE TABLE IF NOT EXISTS guilds (
     id BIGINT PRIMARY KEY,
     prefix nvarchar(10) NOT NULL
-};
+);

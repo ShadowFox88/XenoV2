9cb75cfe77b13c402bd0e3cd770100f91d3bd5f6
Vahin Mehra <vahin@vahin.dev>
2024-04-06 09:06:26 +0000

    Added pgadmin to docker compose and updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,26 +1,23 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT PRIMARY KEY,
-    prefix varchar(10) NOT NULL default 'x-'
-);
-
 DO $$ BEGIN
-    CREATE TYPE blacklist_type AS ENUM ('user', 'guild');
+    CREATE TYPE entity_type AS ENUM ('user', 'guild');
 EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;
 
-CREATE TABLE IF NOT EXISTS blacklist (
-    id BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
-    name text NOT NULL,
-    type blacklist_type NOT NULL,
-    blacklisted_on timestamp with time zone NOT NULL default now(),
-    blacklisted_until timestamp with time zone NOT NULL default now() + interval '3 years',
-    blacklist_active boolean NOT NULL default true
+CREATE TABLE IF NOT EXISTS entities (
+    id BIGINT PRIMARY KEY,
+    prefix varchar(10) NOT NULL default 'x-',
+    type entity_type NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS guilds (
-    id BIGINT PRIMARY KEY,
-    prefix varchar(10) NOT NULL
+CREATE TABLE IF NOT EXISTS blacklist (
+    id BIGINT PRIMARY KEY REFERENCES entities(id) ON DELETE CASCADE,
+    name text NOT NULL,
+    type entity_type NOT NULL,
+    blacklist_reason text NOT NULL default 'No reason provided',
+    blacklisted_on timestamp with time zone NOT NULL default now(),
+    blacklisted_until timestamp with time zone NOT NULL default now() + interval '888 years', -- Lifetime by default
+    blacklist_active boolean NOT NULL default true
 );
 
 DO $$ BEGIN
@@ -40,6 +37,6 @@ END$$;
 CREATE TABLE IF NOT EXISTS user_channels (
     owner BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     channel_id BIGINT NOT NULL,
-    pit_type pit_type NOT NULL,
+    channel_type channel_type NOT NULL,
     archive_mode archive_mode
 )

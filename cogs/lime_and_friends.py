7c0244fa8d94d6a31b65526745f96d36b185ca67
Vahin Mehra <vahin@vahin.dev>
2024-11-23 23:20:31 +0000

    Added a check for the maximum time allowed for timeout

--- cogs/lime_and_friends.py
+++ cogs/lime_and_friends.py
@@ -81,6 +81,9 @@ class Lime_And_Friends(commands.Cog):
                 
         if not any(time.values()):
             raise commands.BadArgument("Please provide a time to timeout the user for.")
+        
+        if datetime.timedelta(**time) > datetime.timedelta(weeks=4):
+            raise commands.BadArgument("You can only timeout a user for a maximum of 4 weeks.")
               
         await user.timeout(datetime.timedelta(**time))
                 
@@ -88,7 +91,6 @@ class Lime_And_Friends(commands.Cog):
             description=f"Timed out {user.mention} until {discord.utils.format_dt(datetime.datetime.utcnow() + datetime.timedelta(**time))}",
             color=discord.Color.green()
         )
-                
         
         await ctx.message.add_reaction(self.bot.emoji_list["animated_green_tick"])
         await ctx.send(embed=embed)

4a0b53c1cbc5b7a8d2090782b4b595207567c2b5
Vahin Mehra <vahin@vahin.dev>
2024-11-24 00:31:15 +0000

    fix incorrectly pasted code

--- cogs/lime_and_friends.py
+++ cogs/lime_and_friends.py
@@ -136,8 +136,8 @@ class Lime_And_Friends(commands.Cog):
             
         embed.timestamp = discord.utils.utcnow()
         embed.set_footer(
-            text=f"Command ran by {self.author.display_name}",
-            icon_url=self.author.display_avatar.url,
+            text=f"Command ran by {ctx.author.display_name}",
+            icon_url=ctx.author.display_avatar.url,
         )
 
         await ctx.message.add_reaction(self.bot.emoji_list["animated_green_tick"])
